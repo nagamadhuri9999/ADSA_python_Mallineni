36a23ec9eb222804ab3e7f18666691e2efc74471
Venkatesh M <127928328+venkatesh7975@users.noreply.github.com>
2026-06-15 12:25:16 +0530

    Update quicksort.py

---
+++ Day10/quicksort.py
@@ -0,0 +1,17 @@
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+def quickSort(arr, low=0, high=None):
+    if high is None:
+        high = len(arr) - 1
+    if low < high:
+        pi = partition(arr, low, high)
+        quickSort(arr,low, pi - 1)
+        quickSort(arr, pi + 1, high)
+    return arr
