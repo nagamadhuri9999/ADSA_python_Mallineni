81781f170c5a63e5d4e13ebf4cc48faecbac9881
venkatesh7975 <venkateshmoyya7975@gmail.com>
2026-06-23 15:13:38 +0530

    Update Day17/adj_list.py

--- Day17/adj_list.py
+++ Day17/adj_list.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 n = 5
 
 adj = [[] for _ in range(n)]
@@ -12,5 +14,68 @@ for u, v in edges:
     adj[u].append(v)
     adj[v].append(u)
 
+print("Adjacency List:")
+
 for i in range(n):
     print(i, "->", adj[i])
+
+
+# ---------------- DFS ----------------
+
+def dfs(start):
+
+    visited = [False] * n
+
+    stack = [start]
+
+    print("\nDFS Traversal:")
+
+    while stack:
+
+        node = stack.pop()
+
+        if not visited[node]:
+
+            visited[node] = True
+
+            print(node, end=" ")
+
+            for neighbor in reversed(adj[node]):
+
+                if not visited[neighbor]:
+                    stack.append(neighbor)
+
+    print()
+
+
+# ---------------- BFS ----------------
+
+def bfs(start):
+
+    visited = [False] * n
+
+    q = deque([start])
+
+    visited[start] = True
+
+    print("BFS Traversal:")
+
+    while q:
+
+        node = q.popleft()
+
+        print(node, end=" ")
+
+        for neighbor in adj[node]:
+
+            if not visited[neighbor]:
+
+                visited[neighbor] = True
+
+                q.append(neighbor)
+
+    print()
+
+
+dfs(0)
+bfs(0)
